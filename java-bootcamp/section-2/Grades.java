5e01db0fcf437ae8abcbb3f9f454535de50a87d0
mohammadmahdi <mohhammadmahdi.4@gmail.com>
2026-06-16 18:42:26 +0330

    Section-2

---
+++ java-bootcamp/section-2/Grades.java
@@ -0,0 +1,13 @@
+public class Grades {
+    public static void main(String[] args){
+        char potions = 'A';
+        char arithmancy = 'B';
+        char charms = 'C';
+
+        System.out.println("potion " + potions);
+        System.out.println("arithmancy " + arithmancy);
+        System.out.println("charms " + charms);
+
+
+    }
+}
---
+++ java-bootcamp/section-2/WholeNumbers.java
@@ -0,0 +1,8 @@
+public class WholeNumbers {
+    public static void main(String[] args){
+        long globalPopulation = 8000000000L;
+        System.out.println("the gobal population is " + globalPopulation);
+        long dailyGoogleSearches = 8500000000L;
+        System.out.println("Daily, there are " + dailyGoogleSearches + " google searches");
+    }
+}
